8bcb174cd5fde01b95e654c98fe8a5e51325f4d0
brian <brian_hollowed@hotmail.com>
2017-06-23 23:16:39 +0100

    update3

--- app.js
+++ app.js
@@ -3,11 +3,14 @@
 
 angular.module('LunchCheck', [])
 
-.controller('LunchCheckController', LunchCheckController {
-	LunchCheckController.$inject = ['$scope', '$filter'];
-function LunchCheckController($scope,filter)
+.controller('LunchCheckController', LunchCheckController) {
+	//LunchCheckController.$inject = ['$scope', '$filter'];
+function LunchCheckController($scope)
 {
-	
+	$scope.method1 = function (){
+		var arrayOfStrings = $scope.name.split(",");
+		$scope.message = arrayOfStrings.length;
+	}
 }
 });
 
